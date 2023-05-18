0252ab2dc15ee4cda03b30343aead4c27e66b586
Kenya <kalvarez@orthocana.com>
2023-05-18 16:59:18 -0600

    adding js file and fixes.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//WHATSSAPP
+let WHATSAPP_BASE = "https://wa.me/";
+let phone_number = "+525582513007";
+let complete_walink = "WHATSAPP_BASE+phone_number";
+console.log(complete_walink);
+
+
+//FACEBOOK
+let MESSENGER_BASE = "https://m.me/";
+let fb_page = "profesortw"
+let complete_messenger = "MESSENGER_BASE+fb_page";
+console.log(complete_messenger);
