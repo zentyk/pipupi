45de8e901ea30f155ef93ba2f6c2237f3bd25650
Kenya <kalvarez@orthocana.com>
2023-05-22 21:13:59 -0600

    Ajustes en los botones y socialmedia.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,41 @@
 //WHATSSAPP
-let WHATSAPP_BASE = "https://wa.me/";
-let phone_number = "+525582513007";
-let complete_walink = "WHATSAPP_BASE+phone_number";
-console.log(complete_walink);
+let base = null;
+let link = null;
 
 
 //FACEBOOK
-let MESSENGER_BASE = "https://m.me/";
-let fb_page = "profesortw"
-let complete_messenger = "MESSENGER_BASE+fb_page";
-console.log(complete_messenger);
+function setSocial(id){
+    if(id === "fb"){
+        base = "https://wa.me/";
+    } else {
+        base = "https://m.me/";
+    }
+}
+
+function generateLink (e, num){
+  
+}
+
+function shareLink(){
+    const numbers = document.getElementById('numbers')
+    numbers.addEventListener('click', event =>{
+        if(navigator.share){
+            navigator.share({
+                text: 'mandar a:',
+                url: 'https://www.google.com/'
+        }).then(() =>{
+            console.log('Gracias por compartir!');
+        })
+        .catch((err)=> console.error(err));
+    } else {
+        alert("UPS! ocurrio un error, vuelve a intentarlo.")
+    }
+    })
+}
+
+function copyLink(){
+    let numbers = document.getElementById('numbers');
+    numbers.select();
+    numbers.setSelectionRange(0,99999);
+    document.execCommand('copy');
+}
