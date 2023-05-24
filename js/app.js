cddf9cb5b232aec2af86d2aa305094d69b4a3b81
Kenya <kalvarez@orthocana.com>
2023-05-24 14:59:11 -0600

    fixes in generateLink.

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
-//WHATSSAPP
 let base = null;
 let link = null;
 
-
 //FACEBOOK
-function setSocial(id){
-    if(id === "fb"){
-        base = "https://wa.me/";
-    } else {
-        base = "https://m.me/";
-    }
+function setSocial(social){
+    if(social === "wa") base = "https://m.me/";
+    if(social === "me") base = "https://wa.me/";
 }
 
-function generateLink (e, num){
-  
+
+
+function generateLink(){
+  let num = document.getElementById("num");
+  let link = base + num;
+  window.open(link);
 }
 
-function shareLink(){
-    const numbers = document.getElementById('numbers')
-    numbers.addEventListener('click', event =>{
-        if(navigator.share){
-            navigator.share({
-                text: 'mandar a:',
-                url: 'https://www.google.com/'
-        }).then(() =>{
-            console.log('Gracias por compartir!');
-        })
-        .catch((err)=> console.error(err));
-    } else {
-        alert("UPS! ocurrio un error, vuelve a intentarlo.")
-    }
-    })
-}
 
-function copyLink(){
-    let numbers = document.getElementById('numbers');
-    numbers.select();
-    numbers.setSelectionRange(0,99999);
-    document.execCommand('copy');
-}
+// function shareLink(){
+//     const numbers = document.getElementById('numbers')
+//     numbers.addEventListener('click', event =>{
+//         if(navigator.share){
+//             navigator.share({
+//                 text: 'mandar a:',
+//                 url: 'https://www.google.com/'
+//         }).then(() =>{
+//             console.log('Gracias por compartir!');
+//         })
+//         .catch((err)=> console.error(err));
+//     } else {
+//         alert("UPS! ocurrio un error, vuelve a intentarlo.")
+//     }
+//     })
+// }
+
+// function copyLink(){
+//     let num = document.getElementById('num');
+//     num.select();
+//     num.setSelectionRange(0,99999);
+//     document.execCommand('copy');
+// }
